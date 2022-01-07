d61cf7a835d454098477044343be76aed815e409
zarasuto <zarasuto@gmail.com>
2022-01-07 11:30:33 +0800

    Url to shortlink logic implementation

--- src/main/java/com/example/linkshortener/Services/UrlShorteningServiceImpl.java
+++ src/main/java/com/example/linkshortener/Services/UrlShorteningServiceImpl.java
@@ -18,7 +18,7 @@ public class UrlShorteningServiceImpl implements UrlShorteningService{
     public Url generateShortLink(UrlData url) {
         if(!url.getOriginalUrl().isEmpty()){
             Url urlToPersist = new Url();
-            urlToPersist.setShortenedURL(this.encodeUrl());
+            urlToPersist.setShortenedURL(this.encodeUrl(url.getOriginalUrl()));
             urlToPersist.setOriginalUrl(validateAndFixUrl(url));
             urlToPersist.setCreationDate(LocalDateTime.now());
             urlToPersist.setExpirationDate(setExpiryDate(url.getExpiryHours()));
@@ -35,9 +35,36 @@ public class UrlShorteningServiceImpl implements UrlShorteningService{
         return LocalDateTime.now().plusHours(hours);
     }
 
-    private String encodeUrl() {
-        //Placeholder
-        return "asdf";
+    private String encodeUrl(String input) {
+        String sampleString = "aA1bB2cC3dD4eE5fF6gG7hH8iI9jJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+        char[] sample = sampleString.toCharArray();
+        Integer sampleSize = sampleString.length();
+        char[] chars = input.toCharArray();
+        Integer charsLen = chars.length;
+        int wholeNo = Math.floorDiv(charsLen , 10);
+        Integer remainder = charsLen - wholeNo * 10;
+        char[] shortened = new char[10];
+        Integer i,j;
+        Integer x = 0;
+        for (i = 0; i <= 9; i++) {
+            int temp = 0,temp1 = 0;
+            for (j = 1; j <= wholeNo; j++) {
+                if(remainder !=0) {
+                    temp += chars[x];
+                    temp += chars[x];
+                    x++;
+                    remainder -= 1;
+                }
+
+                temp += chars[x];
+                x++;
+            }
+            temp1 = Math.floorDiv(temp, sampleSize);
+            temp -= temp1 * sampleSize;
+            shortened[i] += sample[temp];
+        }
+        String str= new String(shortened);
+        return(str);
     }
 
     @Override
