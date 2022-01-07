e1b4ffca1c832ddb8d5f12bf3a53838df3d4a8db
zarasuto <zarasuto@gmail.com>
2022-01-07 13:06:47 +0800

    Created expired links cleaner

--- src/main/java/com/example/linkshortener/Controller/MVCController.java
+++ src/main/java/com/example/linkshortener/Controller/MVCController.java
@@ -3,18 +3,25 @@ package com.example.linkshortener.Controller;
 
 import com.example.linkshortener.Model.Url;
 import com.example.linkshortener.Model.UrlData;
+import com.example.linkshortener.Repositories.UrlRepository;
 import com.example.linkshortener.Services.UrlShorteningServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class MVCController {
     @Autowired
     private UrlShorteningServiceImpl urlShorteningService;
+
+    @Autowired
+    private UrlRepository urlRepository;
 
     @GetMapping(value={"/","/generate"})
     public String getForm(Model model){
@@ -30,5 +37,15 @@ public class MVCController {
             model.addAttribute("Url",urlRep);
         }
         return "index";
+    }
+
+    @Scheduled(fixedRate=3600000) // Calls this method every hour
+    public void CheckAndDeleteExpiredLinks(){
+        List<Url> urlList = urlRepository.findALlExpiredLinks();
+        if(urlList!=null){
+            for(Url url:urlList){
+                urlShorteningService.deleteShortLink(url);
+            }
+        }
     }
 }
--- src/main/java/com/example/linkshortener/Repositories/UrlRepository.java
+++ src/main/java/com/example/linkshortener/Repositories/UrlRepository.java
@@ -2,10 +2,16 @@ package com.example.linkshortener.Repositories;
 
 import com.example.linkshortener.Model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface UrlRepository extends JpaRepository<Url,Long> {
 
     public Url findByshortenedURL(String shortenedURL);
 
+    @Query(value="SELECT * FROM url WHERE expiration_date < NOW()",
+            nativeQuery = true)
+    public List<Url> findALlExpiredLinks();
 }
