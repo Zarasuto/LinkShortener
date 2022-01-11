bfa3aa7770caf007cce8b1815b63c0bedee88379
zarasuto <zarasuto@gmail.com>
2022-01-11 18:53:35 +0800

    Fixed authentication test. if the user is logged out

--- src/main/java/com/example/linkshortener/Security/CurrentAuthenticated.java
+++ src/main/java/com/example/linkshortener/Security/CurrentAuthenticated.java
@@ -1,6 +1,7 @@
 package com.example.linkshortener.Security;
 
 import com.example.linkshortener.Model.User;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Component;
 public class CurrentAuthenticated {
 
     public Authentication getAuthentication(){
+        if(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken){
+            return null;
+        }
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
